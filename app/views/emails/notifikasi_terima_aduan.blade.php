9b4da2db3f8deb2403a8c10830fe066b6a1860a4
Winson <13512071@std.stei.itb.ac.id>
2015-02-10 15:18:16 +0700

    Kirim email jika telah menerima aduan di postLapor

---
+++ app/views/emails/notifikasi_terima_aduan.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h2>Notifikasi Penerimaan Laporan</h2>
+      
+      <p>Hi, {{$nama}} </p>
+      <p>Terima kasih karena telah melaporkan keluhan kepada Taman Bandung.</p>
+      <p>Laporan Anda sangat membantu Taman Bandung menjaga taman-taman di Bandung.</p>
+      <br/>
+		<p>Terima kasih,</p>
+      <p>Taman Bandung</p>
+	</body>
+</html>
