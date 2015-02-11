cfa83f347316bb2fcf6f0e6857ff016514e41e46
felicia-christie <waxrain1217@gmail.com>
2015-02-11 15:26:42 +0700

    Added description to taman table seeder

--- app/database/seeds/TamanTableSeeder.php
+++ app/database/seeds/TamanTableSeeder.php
@@ -7,12 +7,13 @@ class TamanTableSeeder extends Seeder {
 
 		$taman = array(
 //foto, alamat, nama
-			['foto' => "urlhere", 'alamat' => "", 'nama' => "Taman Lansia"],
-			['foto' => "urlhere2", 'alamat' => "", 'nama' => "Taman Jomblo"],
-			['foto' => "urlhere3", 'alamat' => "", 'nama' => "Taman Musik Centrum"]
+			['foto' => "urlhere", 'alamat' => "Jalan Cisangkuy", 'nama' => "Taman Lansia", 'deskripsi' => "Sebelum menjadi Taman Lansia, taman ini banyak dikenal sebagai Taman Cilaki atau Taman Cisangkuy."],
+			['foto' => "urlhere2", 'alamat' => "Jalan Cikapayang", 'nama' => "Taman Jomblo",  'deskripsi' => "Namanya mungkin sedikit menyindir orang yang belum memiliki pasangan. Penamaan taman ini memang sesuai dengan pemanfaatan taman yang kerap digunakan dalam aktivitas berkumpul pemuda dan pemudi Bandung. "],
+			['foto' => "urlhere3", 'alamat' => "Jalan Belitung", 'nama' => "Taman Musik Centrum", 'deskripsi' => "Sebuah ruang publik yang didedikasikan untuk kegiatan musik, seni dan olah raga. Ini merupakan salah satu taman tematik yang digagas oleh Bapak Ridwan Kamil, Walikota Bandung-melengkapi taman-taman tematik yang lain seperti Taman Pustaka Bunga, Taman Jombo, Taman Fotografi (Taman Cempaka) dan yang lainnya."]
 			);
 
 		DB::table('taman')->insert($taman);
+
 	}
 }
 
