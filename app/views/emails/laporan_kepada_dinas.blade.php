59aa50bf86dd3101dc0edf6af133762cb7228928
Winson <13512071@std.stei.itb.ac.id>
2015-02-12 09:41:58 +0700

    Tambah templeate laporan

---
+++ app/views/emails/laporan_kepada_dinas.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h2>[Taman Bandung] Laporan Masyarakat</h2>
+      
+      <p>Kapada {{$nama_dinas}}, </p>
+      <p>Kami telah menerima laporan dari masyarakat bahwa terdapat masalah yang terkait dengan dinas Anda.
+         Kami sangat berharap Anda dapat memprosesnya. Berikut ini adalah isi dari laporan.</p>
+      <p>Tanggal:{{$tanggal_laporan}}</p>
+      <p>Taman:{{$nama_taman}}</p>
+      <p>Alamat:{{$alamat_taman}}</p>
+      <p>Isi Aduan:</p>
+      <p>{{$isi_aduan}}</p>
+      <br/>
+		<p>Terima kasih,</p>
+      <p>Taman Bandung</p>
+	</body>
+</html>
