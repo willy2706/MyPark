1e2f861f97f8ef83eb2f888c9a1c504a30250b6a
felicia-christie <waxrain1217@gmail.com>
2015-02-12 10:38:36 +0700

    index fixed and mostly done

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -44,29 +44,7 @@
 			?>
 
 			@endforeach
-			<div class="section">
-				<div>
-					<div>
-						<a href="programs.html"><img src="images/help-out.jpg" alt=""></a>
-						<h2>Taman Pasupati <br></h2>
-							<p>
-								<b>Letak</b> : _____ <br>
-								<b>Deskripsi</b> : The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-							</p>
-					</div>
-				</div>	
-
-				<div>
-					<div>
-						<a href="programs.html"><img src="images/what-we-do.jpg" alt=""></a>
-						<h2>Taman Pasupati <br></h2>
-							<p>
-								<b>Letak</b> : _____ <br>
-								<b>Deskripsi</b> : The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-							</p>
-					</div>
-				</div>
-			</div>	
+				
 			
 		</div>
 	</div>
