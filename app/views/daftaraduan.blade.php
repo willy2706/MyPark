e1662de86d672f57c7ca2259191b083b91906534
ivanaclairineirsan <ivana.clairine@gmail.com>
2015-02-12 11:46:07 +0700

    daftar aduan selesai

--- app/views/daftaraduan.blade.php
+++ app/views/daftaraduan.blade.php
@@ -32,7 +32,7 @@
 				<p>
 					This website template has been designed by <a href="http://www.freewebsitetemplates.com/">Free Website Templates</a> for you, for free. You can replace all this text with your own text. You can remove any link to our website from this website template, you're free to use this website template without linking back to us. If you're having problems editing this website template, then don't hesitate to ask for help on the <a href="http://www.freewebsitetemplates.com/forums/">Forums</a>.
 				</p-->
-				<h2>Daftar aduan taman</h2>
+				<h2><center>Daftar aduan taman</h2> <hr></center>
 				
 
 
@@ -45,6 +45,7 @@
 					<p> Status : {{$da->status}} <br></p>
 					<!--div class = "gambaraduan"></div><br-->
 					<div class="isiaduan">{{$da->deskripsi}}
+					<hr>
 				</div>
 				@endforeach
 
